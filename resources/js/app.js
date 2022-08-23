908a1e9e3048821c03ee795f4bbd0b173f931252
locht <locht@hub-js.com>
2022-08-23 20:39:47 +0700

    install vuejs and taiwind css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+require("flowbite");
+
+import { createApp } from 'vue';
+
+import TestComponent from './components/TestComponent'
+
+const app = createApp({})
+
+app.component('test-view', TestComponent)
+
+app.mount('#app')
