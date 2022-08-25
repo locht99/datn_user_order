ac40942b41c9b4c32531bae73b93692e851a045b
locht <locht@hub-js.com>
2022-08-25 20:51:45 +0700

    database and model user_order

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\api\CreateCartConTroller;
+use App\Http\Controllers\api\ExtensionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/create-cart', [ExtensionController::class, 'createCart']);
