929f04a30927677dcef8269be29ae21c9681cbcb
khaidang1123 <akhai7230@gmail.com>
2022-08-29 17:06:05 +0700

    create pages for user

--- resources/js/components/user/CartComponent.vue
+++ resources/js/components/user/CartComponent.vue
@@ -1,15 +1,414 @@
 <template>
-    Cart coponent
+  <section
+    style="height: 590px; max-height: 590px"
+    class="border rounded-xl shadow-md shadow-gray-400 my-10"
+  >
+
+  <!-- Header cart -->
+    <section
+      class="sticky w-full flex justify-between px-5 py-5 bg-white rounded-xl"
+    >
+      <div class="flex items-center">
+        <img src="/images/trolley.png" alt="" />
+        <b class="mx-2 text-lg">Giỏ hàng</b>
+      </div>
+      <label for="search" class="border-b-2 border-b-gray-400">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <input
+          type="text"
+          id="search"
+          placeholder="Tìm kiếm sản phẩm"
+          class="border-none focus:ring-0"
+        />
+      </label>
+    </section>
+
+    <!-- Content cart -->
+    <section style="max-height: 435px" class="overflow-auto">
+      <section class="py-5 px-5">
+        <div class="rounded-xl shadow-md shadow-gray-400">
+          <div
+            class="
+              px-5
+              py-3
+              flex
+              justify-between
+              items-center
+              bg-gray-100
+              rounded-xl
+            "
+          >
+            <div class="flex items-center">
+              <img src="/images/1688-logo.png" alt="" />
+              <p class="ml-10 text-xl">白沟新城朵黛帽子商行</p>
+            </div>
+            <div class="flex items-center">
+              <label for="kiemhang" class="mx-4 cursor-pointer select-none">
+                <input
+                  type="radio"
+                  id="kiemhang"
+                  name="ff"
+                  class="
+                    mb-1
+                    rounded-md
+                    cursor-pointer
+                    focus:ring-0
+                    w-5
+                    h-5
+                    border-2 border-gray-400
+                  "
+                />
+                <i class="fa-solid fa-box-open mx-2 text-lg text-gray-700"></i>
+                <span class="text-lg font-semibold text-gray-600"
+                  >Kiểm hàng</span
+                >
+              </label>
+
+              <label for="donggo" class="mx-4 cursor-pointer select-none">
+                <input
+                  type="radio"
+                  id="donggo"
+                  name="ff"
+                  class="
+                    mb-1
+                    rounded-md
+                    cursor-pointer
+                    focus:ring-0
+                    w-5
+                    h-5
+                    border-2 border-gray-400
+                  "
+                />
+                <i
+                  class="fa-solid fa-boxes-stacked mx-2 text-lg text-gray-700"
+                ></i>
+                <span class="text-lg font-semibold text-gray-600">Đóng gỗ</span>
+              </label>
+
+              <label for="donggorieng" class="mx-4 cursor-pointer select-none">
+                <input
+                  type="radio"
+                  id="donggorieng"
+                  name="ff"
+                  class="
+                    mb-1
+                    rounded-md
+                    focus:ring-0
+                    w-5
+                    h-5
+                    border-2 border-gray-400
+                  "
+                />
+                <i class="fa-solid fa-box mx-2 text-lg text-gray-700"></i>
+                <span class="text-lg font-semibold text-gray-600"
+                  >Đóng gỗ riêng</span
+                >
+              </label>
+            </div>
+          </div>
+          <div class="w-full flex">
+            <div class="w-3/4 border-r-2">
+              <table class="w-full">
+                <thead class="h-14 border-b-2">
+                  <tr>
+                    <th class="text-left pl-8">
+                      <input
+                        type="checkbox"
+                        name=""
+                        id=""
+                        class="
+                          rounded-md
+                          cursor-pointer
+                          focus:ring-0
+                          w-5
+                          h-5
+                          border-2 border-gray-400
+                        "
+                      />
+                    </th>
+                    <th class="text-xl text-left pl-8">Sản phẩm</th>
+                    <th class="text-xl text-left pl-8">Số lượng</th>
+                    <th class="text-xl text-left pl-8">Tiền hàng</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="items-center">
+                    <td class="pl-8">
+                      <input
+                        type="checkbox"
+                        name=""
+                        id=""
+                        class="
+                          rounded-md
+                          cursor-pointer
+                          focus:ring-0
+                          w-5
+                          h-5
+                          border-2 border-gray-400
+                        "
+                      />
+                    </td>
+                    <td class="pl-8 items-center">
+                      <img src="images/product-demo.png" alt="" class="w-24 py-4 pr-2 float-left" />
+                      <a href="" class="mt-8 underline block">2022春夏新款交叉印字瑜伽芭比裤女 高腰提臀显瘦紧身运动健身裤</a>
+                    </td>
+                    <td class="pl-8">
+                      <input type="text" value="1" class="w-24 rounded-md h-8 border-2 border-gray-400 font-semibold text-lg focus:ring-0">
+                    </td>
+                    <td class="pl-8">
+                      <p class="text-red-500 font-semibold text-xl">¥45</p>
+                      <p class="text-red-500 font-semibold text-xl">171.000 đ</p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="text-xl px-5 w-1/4">
+              <div class="h-14 flex items-center">
+                <span>Tổng tiền: </span>
+                <span class="text-red-500 font-semibold ml-3">171.000 đ</span>
+              </div>
+              <div>
+                <textarea
+                  name=""
+                  id=""
+                  class="
+                    h-28
+                    w-full
+                    rounded-xl
+                    resize-none
+                    border-1 border-gray-400
+                    focus:ring-0
+                  "
+                  placeholder="Chú thích cho đơn hàng..."
+                ></textarea>
+
+                <button class="w-full mt-2 mb-4 rounded-md text-white py-1">
+                  Đặt hàng
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="py-5 px-5">
+        <div class="rounded-xl shadow-md shadow-gray-400">
+          <div
+            class="
+              px-5
+              py-3
+              flex
+              justify-between
+              items-center
+              bg-gray-100
+              rounded-xl
+            "
+          >
+            <div class="flex items-center">
+              <img src="/images/1688-logo.png" alt="" />
+              <p class="ml-10 text-xl">白沟新城朵黛帽子商行</p>
+            </div>
+            <div class="flex items-center">
+              <label for="kiemhang" class="mx-4 cursor-pointer select-none">
+                <input
+                  type="radio"
+                  id="kiemhang"
+                  name="ff"
+                  class="
+                    mb-1
+                    rounded-md
+                    cursor-pointer
+                    focus:ring-0
+                    w-5
+                    h-5
+                    border-2 border-gray-400
+                  "
+                />
+                <i class="fa-solid fa-box-open mx-2 text-lg text-gray-700"></i>
+                <span class="text-lg font-semibold text-gray-600"
+                  >Kiểm hàng</span
+                >
+              </label>
+
+              <label for="donggo" class="mx-4 cursor-pointer select-none">
+                <input
+                  type="radio"
+                  id="donggo"
+                  name="ff"
+                  class="
+                    mb-1
+                    rounded-md
+                    cursor-pointer
+                    focus:ring-0
+                    w-5
+                    h-5
+                    border-2 border-gray-400
+                  "
+                />
+                <i
+                  class="fa-solid fa-boxes-stacked mx-2 text-lg text-gray-700"
+                ></i>
+                <span class="text-lg font-semibold text-gray-600">Đóng gỗ</span>
+              </label>
+
+              <label for="donggorieng" class="mx-4 cursor-pointer select-none">
+                <input
+                  type="radio"
+                  id="donggorieng"
+                  name="ff"
+                  class="
+                    mb-1
+                    rounded-md
+                    focus:ring-0
+                    w-5
+                    h-5
+                    border-2 border-gray-400
+                  "
+                />
+                <i class="fa-solid fa-box mx-2 text-lg text-gray-700"></i>
+                <span class="text-lg font-semibold text-gray-600"
+                  >Đóng gỗ riêng</span
+                >
+              </label>
+            </div>
+          </div>
+          <div class="w-full flex">
+            <div class="w-3/4 border-r-2">
+              <table class="w-full">
+                <thead class="h-14 border-b-2">
+                  <tr>
+                    <th class="text-left pl-8">
+                      <input
+                        type="checkbox"
+                        name=""
+                        id=""
+                        class="
+                          rounded-md
+                          cursor-pointer
+                          focus:ring-0
+                          w-5
+                          h-5
+                          border-2 border-gray-400
+                        "
+                      />
+                    </th>
+                    <th class="text-xl text-left pl-8">Sản phẩm</th>
+                    <th class="text-xl text-left pl-8">Số lượng</th>
+                    <th class="text-xl text-left pl-8">Tiền hàng</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="items-center">
+                    <td class="pl-8">
+                      <input
+                        type="checkbox"
+                        name=""
+                        id=""
+                        class="
+                          rounded-md
+                          cursor-pointer
+                          focus:ring-0
+                          w-5
+                          h-5
+                          border-2 border-gray-400
+                        "
+                      />
+                    </td>
+                    <td class="pl-8 items-center">
+                      <img src="images/product-demo.png" alt="" class="w-24 py-4 pr-2 float-left" />
+                      <a href="" class="mt-8 underline block">2022春夏新款交叉印字瑜伽芭比裤女 高腰提臀显瘦紧身运动健身裤</a>
+                    </td>
+                    <td class="pl-8">
+                      <input type="text" value="1" class="w-24 rounded-md h-8 border-2 border-gray-400 font-semibold text-lg focus:ring-0">
+                    </td>
+                    <td class="pl-8">
+                      <p class="text-red-500 font-semibold text-xl">¥45</p>
+                      <p class="text-red-500 font-semibold text-xl">171.000 đ</p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="text-xl px-5 w-1/4">
+              <div class="h-14 flex items-center">
+                <span>Tổng tiền: </span>
+                <span class="text-red-500 font-semibold ml-3">171.000 đ</span>
+              </div>
+              <div>
+                <textarea
+                  name=""
+                  id=""
+                  class="
+                    h-28
+                    w-full
+                    rounded-xl
+                    resize-none
+                    border-1 border-gray-400
+                    focus:ring-0
+                  "
+                  placeholder="Chú thích cho đơn hàng..."
+                ></textarea>
+
+                <button class="w-full mt-2 mb-4 rounded-md text-white py-1">
+                  Đặt hàng
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </section>
+
+
+  <!-- Footer cart -->
+    <section
+      class="sticky px-10 items-center border-t-2 rounded-bl-xl w-full pt-3"
+    >
+      <span class="font-semibold text-xl text-gray-700"
+        >Tổng thanh toán ( 0 sản phẩm ):
+      </span>
+      <span class="text-red-600 text-xl font-semibold">¥0 ~ 0 đ</span>
+      <button
+        class="mx-10 py-2 px-12 border-none text-xl rounded-md text-white"
+      >
+        Thanh toán
+      </button>
+    </section>
+  </section>
 </template>
 
 <script>
-
-export default({
-    
-})
+export default {};
 </script>
 
 
 <style scoped>
+button {
+  background-color: #ff3f3a;
+}
+table tbody td{
+  max-width: 250px;
+}
 
+::-webkit-scrollbar {
+    width: 20px;
+    height: 20px;
+  }
+
+::-webkit-scrollbar-track {
+    border-radius: 100vh;
+    background: #f7f4ed;
+}
+
+::-webkit-scrollbar-thumb {
+    background: #e0cbcb;
+    border-radius: 100vh;
+    border: 3px solid #f6f7ed;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background: #c0a0b9;
+}
 </style>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
