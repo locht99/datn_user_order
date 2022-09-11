e1496e2ef9914881876f1e4713c5c85e3bdc6a87
locht <locht@hub-js.com>
2022-09-11 17:09:44 +0700

    update sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+const tailwindcss = require("tailwindcss");
+
 
 /*
  |--------------------------------------------------------------------------
@@ -13,7 +15,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .postCss('resources/css/app.css', 'public/css', [
-        require("tailwindcss"),
-    ]);
+    .sass("resources/sass/app.scss", "public/css")
+    .version()
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss("./tailwind.config.js")],
+    });
 mix.disableNotifications()
