d1d65c8b4566ebfa78f5b088f1533330716537d8
Đinh Đức Dương <dinhducduong2002@outlook.com.vn>
2022-12-13 22:29:55 +0700

    Update OrderComponent.vue

--- resources/js/components/user/OrderComponent.vue
+++ resources/js/components/user/OrderComponent.vue
@@ -188,17 +188,6 @@
                                         }}</span>
                                     </div>
                                 </div>
-                                <div
-                                    class="flex items-center justify-between w-full py-1 text-sm font-semibold border-b border-gray-300 lg:py-3 lg:px-3 text-heading last:border-b-0 last:text-base last:pb-0">
-                                    <div>
-                                        Tổng tiền (tạm tính):
-                                    </div>
-                                    <div>
-                                        <span class="ml-2 text-red-500">{{
-                                                formatPrice(this.listInfo['total_price_order'])
-                                        }}</span>
-                                    </div>
-                                </div>
 
                                 <div
                                     class="flex items-center justify-between w-full py-1 text-sm font-semibold border-b border-gray-300 lg:py-3 lg:px-3 text-heading last:border-b-0 last:text-base last:pb-0">
@@ -208,6 +197,17 @@
                                     <div>
                                         <span class="ml-2 text-red-500">{{
                                                 formatPrice(this.listInfo['deposit_amount'])
+                                        }}</span>
+                                    </div>
+                                </div>
+                                <div
+                                    class="flex items-center justify-between w-full py-1 text-sm font-semibold border-b border-gray-300 lg:py-3 lg:px-3 text-heading last:border-b-0 last:text-base last:pb-0">
+                                    <div>
+                                        Tổng tiền (tạm tính):
+                                    </div>
+                                    <div>
+                                        <span class="ml-2 text-red-500">{{
+                                                formatPrice(this.listInfo['total_price_order'])
                                         }}</span>
                                     </div>
                                 </div>
