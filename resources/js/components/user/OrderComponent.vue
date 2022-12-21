0d638b30a77a79669ef8d2712cc10061a7b721d9
Huy20102002 <huy.dev.2010@gmail.com>
2022-12-21 22:35:48 +0700

    Fix

--- resources/js/components/user/OrderComponent.vue
+++ resources/js/components/user/OrderComponent.vue
@@ -146,6 +146,16 @@
                                 <div
                                     class="flex items-center justify-between w-full py-1 text-sm font-semibold border-b border-gray-300 lg:py-3 lg:px-3 text-heading last:border-b-0 last:text-base last:pb-0">
                                     <div>
+                                        Tiền hàng:
+                                    </div>
+                                    <div>
+                                        <span class="ml-2">{{ formatPrice(this.listInfo['total_price'])
+                                        }}</span>
+                                    </div>
+                                </div>
+                                <div
+                                    class="flex items-center justify-between w-full py-1 text-sm font-semibold border-b border-gray-300 lg:py-3 lg:px-3 text-heading last:border-b-0 last:text-base last:pb-0">
+                                    <div>
                                         Phí mua hàng:
                                     </div>
                                     <div>
